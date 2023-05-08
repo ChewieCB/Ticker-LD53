5ebd8eff9a0f0394bb257b82ad2a3d431b8478c6
Jack McCaffrey <jackmccaffrey96@gmail.com>
2023-05-08 13:33:10 +0100

    Added timer UI

---
+++ src/ui/main_ui/shift_timer/shift_timer.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@onready var title_text = $MarginContainer/VBoxContainer/MarginContainer/TimerTextContainer/TextContainer/TimerText
+@onready var time_text = $MarginContainer/VBoxContainer/TimerTimeContainer/TimeContainter/Time
+@onready var timer = $Timer
+@onready var animation_player = $AnimationPlayer
+
+
+func _ready():
+	add_to_group("ui/timer")
+	set_timer(120)
+	timer.start()
+
+
+func _physics_process(_delta):
+	time_text.text = update_time_display()
+
+
+func set_timer(time):
+	timer.wait_time = time
+
+
+func update_time_display() -> String:
+	var timer_minutes = (int(timer.time_left) % 3600) / 60
+	var timer_seconds = int(timer.time_left) % 60
+	var time_string = "[center]{minutes}:{seconds}[/center]".format(
+		{
+			"minutes": str(timer_minutes),
+			"seconds": (timer_seconds)
+		}
+	)
+	return time_string
